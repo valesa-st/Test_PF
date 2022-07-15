e5380e15628290686cbdea45c31821b04c0fa40a
Валерия <101348978+valesa-st@users.noreply.github.com>
2022-07-15 18:29:22 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 def testing():
     pytest.driver = webdriver.Chrome('./chromedriver.exe')
     pytest.driver.implicitly_wait(10)
-    # Переходим на страницу авторизаци
+    # Переходим на страницу авторизации
     pytest.driver.get('http://petfriends.skillfactory.ru/login')
     yield
     pytest.driver.quit()
