6fbb39166820011fd5e49f849d5c62def8fee97b
Валерия <101348978+valesa-st@users.noreply.github.com>
2022-07-15 18:23:07 +0300

    Create test_show_my_pets.py

---
+++ tests/test_show_my_pets.py
@@ -0,0 +1,11 @@
+from conftest import *
+
+
+def test_show_my_pets(go_to_my_pets):
+    """Проверка, что мы находимся на странице Мои питомцы"""
+
+    # Нажимаем на ссылку "Мои питомцы"
+    pytest.driver.find_element_by_link_text("Мои питомцы").click()
+
+    # Проверка, что мы находимся на странице "Мои питомцы"
+    assert pytest.driver.current_url == 'https://petfriends.skillfactory.ru/my_pets'
